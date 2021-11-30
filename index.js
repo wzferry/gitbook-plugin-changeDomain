a002c5d63bffddde397c602fe3281cc9c2fce710
王铸 <wangzhu10@jd.com>
2021-11-30 21:35:50 +0800

    修改值获取

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /*
  * @Author: wangzhu
  * @Date: 2021-11-30 20:25:53
- * @LastEditTime: 2021-11-30 21:14:14
+ * @LastEditTime: 2021-11-30 21:35:17
  * @LastEditors: Please set LastEditors
  */
 module.exports = {
   hooks: {
     "init": function() {
       console.log('lala')
-      var domain = this.config.pluginsConfig.domain;
+      var domain = this.config.pluginsConfig.changedomain.domain;
       document.domain = domain
       console.log('init', domain)
     },
     "page:before": function () {
       console.log('eee')
-      var domain = this.config.pluginsConfig.domain;
+      var domain = this.config.pluginsConfig.changedomain.domain;
       document.domain = domain
       console.log('page:before', domain)
     }
