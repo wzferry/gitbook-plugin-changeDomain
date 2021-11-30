61e65cdb4acc6efa214bbcf272c8f569df290f01
王铸 <wangzhu10@jd.com>
2021-11-30 23:37:30 +0800

    修改钩子函数返回值

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wangzhu
  * @Date: 2021-11-30 20:25:53
- * @LastEditTime: 2021-11-30 21:35:17
+ * @LastEditTime: 2021-11-30 23:37:04
  * @LastEditors: Please set LastEditors
  */
 module.exports = {
@@ -12,11 +12,12 @@ module.exports = {
       document.domain = domain
       console.log('init', domain)
     },
-    "page:before": function () {
+    "page:before": function (page) {
       console.log('eee')
       var domain = this.config.pluginsConfig.changedomain.domain;
       document.domain = domain
       console.log('page:before', domain)
+      return page
     }
   }
 };
